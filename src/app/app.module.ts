5cc93c948fb67780bd186bb7f15992195b8fd8df
chendaliang <piaoyaocdl@163.com>
2020-08-03 17:02:50 +0800

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {DengluyeComponent} from "../page/dengluye/dengluye.component";
-import {NzButtonModule, NzCardModule, NzInputModule, NzNotificationServiceModule} from "ng-zorro-antd";
+import {
+    NzButtonModule,
+    NzCardModule,
+    NzInputModule,
+    NzNotificationModule,
+    NzNotificationServiceModule
+} from "ng-zorro-antd";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 
@@ -21,6 +27,7 @@ import {HttpClientModule} from "@angular/common/http";
         FormsModule,
         NzButtonModule,
         HttpClientModule,
+        NzNotificationModule
     ],
     providers: [],
     bootstrap: [AppComponent]
