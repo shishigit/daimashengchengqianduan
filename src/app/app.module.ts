e69beb215847fe16ffed588ecd6df4e440253ab0
chendaliang <piaoyaocdl@163.com>
2020-08-07 11:44:15 +0800

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,8 @@ import {ShujukuliebiaoComponent} from './zujian/shujukuliebiao/shujukuliebiao.co
 import {HongtiandaimaComponent} from './page/hongtian/hongtiandaima/hongtiandaima.component';
 import {ShujukutableComponent} from './zujian/shujukutable/shujukutable.component';
 import {JjytsXiangmuComponent} from './page/jjyts/jjyts-xiangmu/jjyts-xiangmu.component';
-import {JjytsDaimaComponent} from "./page/jjyts/jjyts-daima/jjyts-danbiaoshengcheng/jjyts-daima.component";
+import {JjytsDaimaComponent} from "./page/jjyts/jjyts-daima/jjyts-daima.component";
+import {JjytsKubiaoComponent} from './page/jjyts/jjyts-kubiao/jjyts-kubiao.component';
 
 @NgModule({
     declarations: [
@@ -46,7 +47,8 @@ import {JjytsDaimaComponent} from "./page/jjyts/jjyts-daima/jjyts-danbiaoshengch
         HongtiandaimaComponent,
         ShujukutableComponent,
         JjytsXiangmuComponent,
-        JjytsDaimaComponent
+        JjytsDaimaComponent,
+        JjytsKubiaoComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/page/jjyts/jjyts-kubiao/jjyts-kubiao.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-jjyts-kubiao',
+    templateUrl: './jjyts-kubiao.component.html',
+    styleUrls: ['./jjyts-kubiao.component.css']
+})
+export class JjytsKubiaoComponent implements OnInit
+{
+
+    constructor()
+    {
+    }
+
+    ngOnInit(): void
+    {
+    }
+
+}
